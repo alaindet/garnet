a87c942a8f6a6a3bcdc75d96405b2a4c74204772
Alain D'Ettorre <alain.det@gmail.com>
2021-08-06 09:37:28 +0200

    Add tests routes on development only

--- backend/src/routes.php
+++ backend/src/routes.php
@@ -3,14 +3,19 @@
 use App\Core\Routing\RouteGroup;
 use App\Core\Middleware\CorsMiddleware;
 use App\Core\Middleware\AuthenticationMiddleware;
-use App\Features\Todos\Routes as TodosRoutes;
+// use App\Features\Todos\Routes as TodosRoutes;
 use App\Features\Tests\Routes as TestsRoutes;
 
 $routes = [
     // TodosRoutes::register(),
-    TestsRoutes::register(),
+
     // ...
 ];
+
+// Add test routes
+if (!appConfig('env.production')) {
+    $routes[] = TestsRoutes::register();
+}
 
 $middleware = [
     CorsMiddleware::class,
